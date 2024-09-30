47842320cc6c4839c5564eaf2a2555099e2c8a28
karen23v <71695800@institutocajas.info>
2024-09-30 19:27:46 +0200

    Update ejercicio1.dart

--- semana3/ejercicio1.dart
+++ semana3/ejercicio1.dart
@@ -1,3 +1,5 @@
+//Diseñe un programa en Lenguaje Dart que determine el área total (A) y el volumen (V) de un cilindro del que se conoce su radio (r) y su altura (h). Considere las siguientes fórmulas: A = 2πr(r+h) y V = π r²h//
+
 import 'dart:io';
 import 'dart:math'; 
 
