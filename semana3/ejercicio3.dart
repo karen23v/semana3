5a8887d8df4c79a45f9d651408a3a91693a08752
karen23v <71695800@institutocajas.info>
2024-09-30 19:32:49 +0200

    Update ejercicio3.dart

--- semana3/ejercicio3.dart
+++ semana3/ejercicio3.dart
@@ -1,3 +1,8 @@
+//La empresa "Juanito Import" SAC expondrá sus productos en una feria. La empresa considera que el monto total de dinero a invertir estará distribuido de la siguiente manera: 
+//Alquiler de espacio en la feria (23%), Publicidad (7%), Transporte (26%), Servicios feriales (12%), Decoración (21%), Gastos varios (11%). Dado el monto total de dinero a 
+//invertir, implemente un programa que determine cuánto gastará la empresa en cada rubro.
+
+
 import 'dart:io';
 
 void main() {
